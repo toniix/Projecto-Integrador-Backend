2c5a14c699f88047612b1776d1e1c5694c9d98b3
benric8 <benric.366@gmail.com>
2025-02-20 10:40:03 -0500

    feat:detalle product y eliminar product

--- src/main/java/com/proyectofinal/clave_compas/controller/ProductController.java
+++ src/main/java/com/proyectofinal/clave_compas/controller/ProductController.java
@@ -3,12 +3,15 @@ package com.proyectofinal.clave_compas.controller;
 
 
 import com.proyectofinal.clave_compas.bd.clavecompas.entities.ProductEntity;
+import com.proyectofinal.clave_compas.bd.clavecompas.repositories.ProductRepository;
+import com.proyectofinal.clave_compas.controller.responses.GlobalResponse;
 import com.proyectofinal.clave_compas.service.ImageServices;
 import com.proyectofinal.clave_compas.service.ProductServices;
 import com.proyectofinal.clave_compas.service.dto.ProductDTO;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -19,19 +22,48 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
 
     private final ProductServices productServices;
-    private final ImageServices imageServices;
-    
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(@RequestParam int page, @RequestParam int pageSize) {
-        return ResponseEntity.ok(productServices.getPaginateProducts(page, pageSize));
+    public ResponseEntity<GlobalResponse> findAll(@RequestParam int page, @RequestParam int pageSize) {
+        GlobalResponse gres = GlobalResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Operación exitosa")
+                .response(productServices.getPaginateProducts(page, pageSize))
+                .build();
+        return ResponseEntity.ok(gres);
     }
     
     @PostMapping
-    private ResponseEntity<ProductEntity> saveProduct(@Validated @RequestBody ProductDTO productDto)  {
+    private ResponseEntity<GlobalResponse> saveProduct(@Validated @RequestBody ProductDTO productDto)  {
+        GlobalResponse gres = GlobalResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Operación exitosa")
+                .response(productServices.saveProduct(productDto))
+                .build();
 
-            ProductEntity productEntity = productServices.saveProduct(productDto);
-            return ResponseEntity.ok(productEntity);
+            return ResponseEntity.ok(gres);
+
+    }
+
+    @GetMapping("/{idProduct}")
+    public ResponseEntity<GlobalResponse> getProductById(@PathVariable Integer idProduct) {
+        GlobalResponse gres = GlobalResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Operación exitosa")
+                .response(productServices.getProductById(idProduct))
+                .build();
+        return ResponseEntity.ok(gres);
+
+    }
+
+    @PutMapping("/{idProduct}")
+    public ResponseEntity<GlobalResponse> deleteProductById(@PathVariable Integer idProduct) {
+        productServices.deleteProductById(idProduct);
+        GlobalResponse gres = GlobalResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Operación exitosa")
+                .build();
+        return ResponseEntity.ok(gres);
 
     }
 }
---
+++ src/main/java/com/proyectofinal/clave_compas/exception/DeleteOperationException.java
@@ -0,0 +1,8 @@
+package com.proyectofinal.clave_compas.exception;
+
+
+public class DeleteOperationException extends RuntimeException {
+    public DeleteOperationException(String message) {
+        super(message);
+    }
+}
